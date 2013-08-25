baf2e2ab5434f36b64f94c550f3dc5187f835d85
walkeriniraq <nathan@rylath.net>
2013-08-25 17:26:34 -0400

    Attempt #2 for favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  config.relative_url_root = ""
-
   def logged_in?
     !current_username.nil?
   end
