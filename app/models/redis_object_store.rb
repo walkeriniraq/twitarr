8a55036ce8c046ed29335387a65b9640e2a7ba43
walkeriniraq <nathan@rylath.net>
2013-09-22 14:40:28 -0400

    Initial code for object store

--- app/models/redis_object_store.rb
+++ app/models/redis_object_store.rb
@@ -1,8 +1,15 @@
 class RedisObjectStore
   def get(clazz, id)
+    data = db.get("#{clazz.to_s}:#{id}")
+    clazz.new(data)
   end
 
   def save(obj)
+    db.set("#{obj.class.to_s}:#{id}", obj.to_hash(obj.class.fattrs))
+  end
+
+  def delete(clazz, id)
+    db.del("#{clazz.to_s}:#{id}")
   end
 
 end
