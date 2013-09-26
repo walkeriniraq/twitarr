dfa076fc4e38557ac4fd0abd3dd55d2113c3514a
walkeriniraq <nathan@rylath.net>
2013-09-25 21:59:04 -0400

    Admin controller switch

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,9 @@
 class AdminController < ApplicationController
+
+  def users_set
+    Redis::Set.new('System:users')
+  end
+
   def no_access
     return login_required unless logged_in?
     return render_json status: 'Restricted to admin.' unless is_admin?
@@ -11,59 +16,60 @@ class AdminController < ApplicationController
 
   def users
     return no_access unless has_access?
-    render_json status: 'ok', list: User.list_usernames.map { |x| User.get(x).to_hash :username, :is_admin, :status, :email, :empty_password }
+    render_json status: 'ok', list: UserDecorator.decorate_collection(object_store.get(User, users_set.members)).map { |x| x.admin_hash }
   end
 
   def find_user
     return no_access unless has_access?
-    return render_json status: 'User does not exist.' unless User.exist? params[:username]
-    render_json status: 'ok', user: User.get(params[:username]).to_hash([:username, :is_admin, :status, :email, :empty_password])
+    user = object_store.get(User, params[:username])
+    return render_json status: 'User does not exist.' unless user
+    render_json status: 'ok', user: user.decorate.admin_hash
   end
 
   def update_user
     return no_access unless has_access?
-    return render_json status: 'User does not exist.' unless User.exist? params[:username]
-    user = User.get params[:username]
+    user = object_store.get(User, params[:username])
+    return render_json status: 'User does not exist.' unless user
     user.is_admin = params[:is_admin] == 'true'
     # don't let the user turn off his own admin status
     user.is_admin = true if user.username == current_username
     user.status = params[:status]
     user.email = params[:email]
-    user.save
+    object_store.save(user, user.username)
     render_json status: 'ok'
   end
 
   def add_user
-    return no_access unless has_access?
-    return render_json status: 'Username already exists.' if User.exist? params[:username]
-    return render_json status: 'Username cannot contain spaces or the following characters: %#:' unless User.valid_username? params[:username]
-    return render_json status: 'Username must be at least six characters.' if params[:username].nil? || params[:username].length < 6
-    user = User.new
-    user.username = params[:username]
-    user.is_admin = params[:is_admin] == 'true'
-    user.status = params[:status]
-    user.email = params[:email]
-    user.save
+    #return no_access unless has_access?
+    #return render_json status: 'Username already exists.' if User.exist? params[:username]
+    #return render_json status: 'Username cannot contain spaces or the following characters: %#:' unless User.valid_username? params[:username]
+    #return render_json status: 'Username must be at least six characters.' if params[:username].nil? || params[:username].length < 6
+    #user = User.new
+    #user.username = params[:username]
+    #user.is_admin = params[:is_admin] == 'true'
+    #user.status = params[:status]
+    #user.email = params[:email]
+    #user.save
+    # TODO: need context
     render_json status: 'ok'
   end
 
   def activate
     return no_access unless has_access?
     return render_json status: 'User does not exist.' unless User.exist? params[:username]
-    user = User.get params[:username]
+    user = object_store.get(User, params[:username])
     user.status = 'active'
-    user.save
+    object_store.save(user, user.username)
     render_json status: 'ok'
   end
 
   def reset_password
     return no_access unless has_access?
-    puts "USERNAME: " + params[:username]
     return render_json status: 'User does not exist.' unless User.exist? params[:username]
     return render_json status: 'Password must be at least six characters long.' if params[:new_password].length < 6
-    user = User.get params[:username]
+    user = object_store.get(User, params[:username])
     user.password = BCrypt::Password.create params[:new_password]
-    user.save
+    object_store.save(user, user.username)
     render_json status: 'ok'
   end
 
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,8 +1,12 @@
-class UserDecorator < Draper::Decorator
+class UserDecorator < DraperDecorator
   delegate_all
 
   def gui_hash
     to_hash %w(username is_admin friends)
+  end
+  
+  def admin_hash
+    to_hash %w(username is_admin status email empty_password)
   end
 
 end
