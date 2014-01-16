7f13a5dcf9138085f46b7cf08a305eaaee28f96f
walkeriniraq <nathan@rylath.net>
2014-01-16 09:10:41 -0600

    Session cookie?!

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Twitarr::Application.config.session_store :cookie_store, key: '_twitarr_session'
+Twitarr::Application.config.session_store :cookie_store, {
+    key: '_twitarr_session',
+    expire_after: 3.days
+}
