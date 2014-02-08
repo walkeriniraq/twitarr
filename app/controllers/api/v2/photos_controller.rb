7f9fb57ba4a18b91480d44ef5b6c8f85c137810d
walkeriniraq <nathan@rylath.net>
2014-02-08 11:31:54 -0600

    Fixes #100 - I'm almost certain the photos api is right this time.

--- app/controllers/api/v2/photos_controller.rb
+++ app/controllers/api/v2/photos_controller.rb
@@ -7,11 +7,18 @@ class API::V2::PhotosController < BaseRedisController
     list = redis.photo_list[page * PAGE_LENGTH, PAGE_LENGTH]
     photos = redis.photo_metadata_store.get(list).reject { |x| x.post_id.nil? }
     render_json(status: 'no more items') and return if photos.blank?
-    data = photos.map { |x| { photo: x.store_filename } }
-    redis.post_store.get(photos.map { |x| x.post_id }).each_with_index do |post, idx|
-      data[idx][:time] = post.post_time
-      data[idx][:username] = post.username
-      data[idx][:message] = post.message
+    posts = redis.post_store.get(photos.map { |x| x.post_id }).reduce({}) do |hash, post|
+      hash[post.post_id] = post
+      hash
+    end
+    data = photos.map do |photo|
+      post = posts[photo.post_id]
+      {
+          photo: photo.store_filename,
+          time: post.andand.post_time,
+          username: post.andand.username,
+          message: post.andand.message
+      }
     end
     render_json status: 'ok',
                 total_count: redis.photo_list.size,
