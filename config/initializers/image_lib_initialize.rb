6e1ef9e8f3c6a3b49ab951ae208b8847de8cd520
James Stapleton <github@tasermonkeys.com>
2014-11-30 20:32:55 -0500

    For right now, lets just tag images as animated or not.  And then let the client application do something about it.

--- config/initializers/image_lib_initialize.rb
+++ config/initializers/image_lib_initialize.rb
@@ -6,4 +6,11 @@ require_relative '../../lib/twelvemonkeys-imageio-core-3.0-SNAPSHOT.jar'
 require_relative '../../lib/twelvemonkeys-imageio-jpeg-3.0-SNAPSHOT.jar'
 require_relative '../../lib/twelvemonkeys-imageio-metadata-3.0-SNAPSHOT.jar'
 require_relative '../../lib/twelvemonkeys-imageio-tiff-3.0-SNAPSHOT.jar'
+
+require_relative '../../lib/imagehelpers-1.0-SNAPSHOT.jar'
+
+module ImageHelpers
+  java_import 'com.tasermonkeys.imagehelpers.AnimatedImage'
+end
+
 # FIXES FOR STUPID JAVA IMAGE PROCESSING
