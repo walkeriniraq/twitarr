b8f7cbd614b8882aecde3049370a5ae618b8988a
walkeriniraq <nathan@rylath.net>
2014-12-07 21:16:58 -0600

    Set the identicon if no photo exists

--- app/controllers/api/v2/user_controller.rb
+++ app/controllers/api/v2/user_controller.rb
@@ -4,11 +4,11 @@ class API::V2::UserController < ApplicationController
   def auth
     login_result = validate_login params[:username], params[:password]
     if login_result.has_key? :error
-      render json: {:status => 'incorrect password or username'}, status: 401 and return
+      render json: { :status => 'incorrect password or username' }, status: 401 and return
     else
       @user = login_result[:user]
       login_user @user
-      render json: {:status => 'ok', :key => build_key(@user.username)}
+      render json: { :status => 'ok', :key => build_key(@user.username) }
     end
   end
 
@@ -30,18 +30,22 @@ class API::V2::UserController < ApplicationController
     return unless logged_in!
     user = User.where(username: params[:username]).first
     unless user
-      render status: :not_found, json: {status: 'Not found', error: "User #{params[:username]} is not found."}
+      render status: :not_found, json: { status: 'Not found', error: "User #{params[:username]} is not found." }
       return
     end
     if current_user.is_admin && params[:admin]
-      render status: :ok, json: {status: 'Found', user: UserDecorator.decorate(user).admin_hash}
+      render status: :ok, json: { status: 'Found', user: UserDecorator.decorate(user).admin_hash }
     else
-      render status: :ok, json: {status: 'Found', user: UserDecorator.decorate(user).gui_hash}
+      render status: :ok, json: { status: 'Found', user: UserDecorator.decorate(user).gui_hash }
     end
   end
 
   def get_photo
     user = User.where(username: params[:username]).first
+    unless (File.exists? user.profile_picture_path)
+      user.set_profile_image_as_identicon
+      user.save
+    end
     send_file user.profile_picture_path, disposition: 'inline'
   end
 
@@ -49,7 +53,7 @@ class API::V2::UserController < ApplicationController
     return unless logged_in!
 
     if params[:username] != current_user and not current_user.is_admin
-      render json: {message: 'Unable to modify another user\'s profile picture.', status: 'err'}, status: :forbidden
+      render json: { message: 'Unable to modify another user\'s profile picture.', status: 'err' }, status: :forbidden
       return
     end
 
@@ -59,7 +63,7 @@ class API::V2::UserController < ApplicationController
       user = current_user
     end
     unless user
-      render status: :not_found, json: {status: 'Not found', error: "User #{params[:username]} is not found."}
+      render status: :not_found, json: { status: 'Not found', error: "User #{params[:username]} is not found." }
       return
     end
     user.set_profile_image_as_identicon
@@ -68,10 +72,10 @@ class API::V2::UserController < ApplicationController
   def update_photo
     return unless logged_in!
     puts params
-    return render json: {message: 'Must provide a photo to upload.', status: 'err'}, status: :bad_request unless params[:file]
+    return render json: { message: 'Must provide a photo to upload.', status: 'err' }, status: :bad_request unless params[:file]
 
     if params[:username] != current_username and not current_user.is_admin
-      render json: {message: 'Unable to modify another user\'s profile picture.', status: 'err'}, status: :forbidden
+      render json: { message: 'Unable to modify another user\'s profile picture.', status: 'err' }, status: :forbidden
       return
     end
 
@@ -81,23 +85,23 @@ class API::V2::UserController < ApplicationController
       user = current_user
     end
     unless user
-      render status: :not_found, json: {status: 'Not found', error: "User #{params[:username]} is not found."}
+      render status: :not_found, json: { status: 'Not found', error: "User #{params[:username]} is not found." }
       return
     end
 
     user.profile_picture_from_file params[:file]
-    render json:{status:'ok', message:'updated user\'s profile picture'}
+    render json: { status: 'ok', message: 'updated user\'s profile picture' }
   end
 
   def reset_mentions
     return unless logged_in!
     current_user.reset_mentions
-    render status: :ok, json: {status: 'OK', user: UserDecorator.decorate(current_user).self_hash}
+    render status: :ok, json: { status: 'OK', user: UserDecorator.decorate(current_user).self_hash }
   end
 
   def mentions
     return unless logged_in!
-    render status: :ok, json: {mentions: current_user.unnoticed_mentions}
+    render status: :ok, json: { mentions: current_user.unnoticed_mentions }
   end
 
   def likes
@@ -106,7 +110,7 @@ class API::V2::UserController < ApplicationController
     skip = params[:skip] || 0
     query = current_user.liked_posts.limit(limit).skip(skip)
     count = query.length
-    result = [status: 'ok', user:current_user.username, total_count:count, next:(skip + limit), items:query.length, likes:query]
+    result = [status: 'ok', user: current_user.username, total_count: count, next: (skip + limit), items: query.length, likes: query]
     respond_to do |format|
       format.json { render json: result }
       format.xml { render xml: result }
