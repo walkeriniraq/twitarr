2a3310877fc794f4d5b6f01f11b42437c2264c8f
James Stapleton <github@tasermonkeys.com>
2015-01-01 23:06:39 -0500

    Refactor to reduce redudent code, and then added the more flag, and allowed for paging

--- app/controllers/api/v2/search_controller.rb
+++ app/controllers/api/v2/search_controller.rb
@@ -1,6 +1,8 @@
 class API::V2::SearchController < ApplicationController
   include Twitter::Extractor
 
+  PAGE_SIZE = 20
+
   # noinspection RailsParamDefResolve
   skip_before_action :verify_authenticity_token
 
@@ -10,19 +12,26 @@ class API::V2::SearchController < ApplicationController
       return
     end
     params[:current_username] = current_username
+    limit = (params[:limit] || PAGE_SIZE).to_i
+    start_loc = (params[:page] || 0).to_i * limit
 
     search_op = StreamPost.build_search_object(params)
-    stream_matches = StreamPost.search(params).map { |e| e.decorate.to_hash(current_username) }
-    forum_matches = Forum.search(params).map { |e| e.decorate.to_meta_hash }
-    users_matches = User.search(params).map { |e| e.decorate.gui_hash }
-    seamail_matches = Seamail.search(params).map { |e| e.decorate.to_meta_hash }
 
-    render json: {stream_posts: {matches: stream_matches, count: stream_matches.length},
-                  forum_posts: {matches: forum_matches, count: forum_matches.length},
-                  users: {matches: users_matches, count: users_matches.length},
-                  seamails: {matches: seamail_matches, count: seamail_matches.length },
+    render json: {stream_posts: do_search(params, StreamPost, limit, start_loc) { |e| e.decorate.to_hash(current_username) },
+                  forum_posts: do_search(params, Forum, limit, start_loc) { |e| e.decorate.to_meta_hash },
+                  users: do_search(params, User, limit, start_loc) { |e| e.decorate.gui_hash },
+                  seamails: do_search(params, Seamail, limit, start_loc) { |e| e.decorate.to_meta_hash },
                   query: {text: search_op[:text],
                           hash_tags: search_op[:hash_tags],
                           screenames: search_op[:screenames]}}
   end
+
+
+  def do_search(params, collection, limit, start_loc, &block)
+    query = collection.search(params).limit(limit).skip(start_loc)
+    matches = query.map { |e| yield e }
+    more = (start_loc + limit < query.length)
+    {matches: matches, count: query.length, more: more}
+  end
+
 end
