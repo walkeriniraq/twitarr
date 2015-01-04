b3fc6bf2d2313dce486ef14df154a72f425c37b9
James Stapleton <github@tasermonkeys.com>
2015-01-04 11:44:11 -0500

    Updating db/seeds.rb to add forum entries.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ puts "Using a base date of #{@BASE_DATE}"
 unless User.exist? 'kvort'
   puts 'Creating user kvort'
   user = User.new username: 'kvort', display_name: 'kvort',
-                  is_admin: true, status: UserController::ACTIVE_STATUS, email: 'kvort@rylath.net',
+                  is_admin: true, status: User::ACTIVE_STATUS, email: 'kvort@rylath.net',
                   security_question: 'none', security_answer: 'none'
   user.set_password 'kvort'
   user.save
@@ -14,7 +14,7 @@ end
 unless User.exist? 'james'
   puts 'Creating user james'
   user = User.new username: 'james', display_name: 'james',
-                  is_admin: false, status: UserController::ACTIVE_STATUS, email: 'james@james.com',
+                  is_admin: false, status: User::ACTIVE_STATUS, email: 'james@james.com',
                   security_question: 'none', security_answer: 'none'
   user.set_password 'james'
   user.save
@@ -22,7 +22,7 @@ end
 unless User.exist? 'steve'
   puts 'Creating user steve'
   user = User.new username: 'steve', display_name: 'steve',
-                  is_admin: false, status: UserController::ACTIVE_STATUS, email: 'james@james.com',
+                  is_admin: false, status: User::ACTIVE_STATUS, email: 'james@james.com',
                   security_question: 'none', security_answer: 'none'
   user.set_password 'steve'
   user.save
@@ -30,7 +30,7 @@ end
 unless User.exist? 'admin'
   puts 'Creating user admin'
   user = User.new username: 'admin', display_name: 'admin',
-                  is_admin: true, status: UserController::ACTIVE_STATUS, email: 'admin@james.com',
+                  is_admin: true, status: User::ACTIVE_STATUS, email: 'admin@james.com',
                   security_question: 'none', security_answer: 'none'
   user.set_password 'admin'
   user.save
@@ -75,7 +75,6 @@ Dir.mktmpdir do |dir|
   photos.push add_photo('http://lorempixel.com/1900/1200/cats/8/', File.join(dir, 'tired_cat.jpg'), 'james', at_time(12, 5))
   photos.push add_photo('http://i.imgur.com/FJdle9E.jpg', File.join(dir, 'warm_bread.jpg'), 'kvort', at_time(11, 15))
 end
-
 def create_post(text, author, timestamp, photo, likes = [])
   post = StreamPost.create(text: text, author: author, timestamp: timestamp, photo: photo)
   unless post.valid?
@@ -89,8 +88,7 @@ def create_post(text, author, timestamp, photo, likes = [])
   post
 end
 
-StreamPost.delete_all
-
+#StreamPost.delete_all
 if StreamPost.count == 0
   create_post 'This is a cute cat #catphotos', 'james', at_time(14, 31, offset:-1.day), photos[0].id, ['steven']
   create_post 'Joco is the best cruise ever #jococruise', 'steve', at_time(10, 9), nil
@@ -132,4 +130,68 @@ if StreamPost.count == 0
   create_post '@kvort though you can never eat to much #cookies', 'james', at_time(11, 30), nil, %w(steven)
   create_post '@james challenge accepted.', 'kvort', at_time(11, 31), nil, %w(james steven)
   create_post 'Whats the over/under?', 'steve', at_time(11, 32), nil, %w(kvort james steven)
+end
+
+forum_photos = []
+Dir.mktmpdir do |dir|
+  forum_photos.push add_photo('http://lorempixel.com/1900/1200/cats/9/', File.join(dir, 'forum1_init_cat.jpg'), 'kvort', at_time(8, 5))
+  forum_photos.push add_photo('http://lorempixel.com/1900/1200/cats/10/', File.join(dir, 'forum1_init2_cat.jpg'), 'kvort', at_time(8, 6))
+  forum_photos.push add_photo('http://lorempixel.com/1900/1200/cats/11/', File.join(dir, 'forum1_post1_cat.jpg'), 'james', at_time(8, 15))
+  forum_photos.push add_photo('http://lorempixel.com/1900/1200/food/1/', File.join(dir, 'forum2_init1_food.jpg'), 'james', at_time(8, 20))
+  forum_photos.push add_photo('http://lorempixel.com/1900/1200/food/3/', File.join(dir, 'forum2_post2_food.jpg'), 'steve', at_time(8, 21))
+  forum_photos.push add_photo('http://lorempixel.com/1900/1200/food/7/', File.join(dir, 'forum2_post3_bread.jpg'), 'kvort', at_time(8, 22))
+end
+
+def create_forum(subject, text, author, timestamp, photos)
+  #force photos to be an array!
+  photos = [photos] unless photos.is_a? Array
+  photos = photos.map { |p| p.id.to_str }
+  forum = Forum.create_new_forum(author, subject, text, photos)
+  forum.posts.first.timestamp = timestamp
+  forum.save!
+  forum
+end
+
+def add_forum_post(forum, text, author, timestamp, photos)
+  #force photos to be an array!
+  photos = [photos] unless photos.is_a? Array
+  photos = photos.map { |p| p.id.to_str }
+  post =  forum.add_post author, text, photos
+  post.timestamp = timestamp
+  post.save!
+  post
+end
+
+Forum.delete_all
+if Forum.count == 0
+  f = create_forum 'First forum', 'Hey guys this is the first forum entry. Lets talk about some #catphotos. ' +
+                   '@james you should post some.', 'kvort', at_time(8, 6), [forum_photos[0], forum_photos[1]]
+  add_forum_post f, '@kvort Alright here you go! #yetanothercat', 'james', at_time(8, 15), forum_photos[2]
+  f = create_forum 'Food pictures are awesome', 'Hey guys lets post some of the awesome food here. #foodftw',
+      'james', at_time(8, 20), forum_photos[3]
+  add_forum_post f, '@james I <3 food', 'steve', at_time(8, 20), forum_photos[4]
+  add_forum_post f, '@steve @james I think this needs some #warmbread', 'kvort', at_time(8, 22), forum_photos[5]
+end
+
+def create_seamail(subject, text, author, recipients, timestamp)
+  seamail = Seamail.create_new_seamail author, recipients, subject, text
+  seamail.last_update = timestamp
+  seamail.messages.first.timestamp = timestamp
+  seamail.save!
+  seamail
+end
+def reply_seamail(seamail, text, author, timestamp)
+  message = seamail.add_message author, text
+  message.timestamp = timestamp
+  message.save!
+  message
+end
+Seamail.delete_all
+if Seamail.count == 0
+  seamail = create_seamail 'Hey lets meet up', 'How about at 10:30?', 'james', ['kvort'], at_time(8, 23)
+  reply_seamail seamail, 'Alright, 10-forward?', 'kvort', at_time(8, 26)
+  reply_seamail seamail, 'Sounds great to me!', 'james', at_time(8, 28)
+  seamail = create_seamail 'artemis?', 'We should go to the game room and play artemis at 15:00!', 'steve', ['kvort', 'james'], at_time(9, 23)
+  reply_seamail seamail, 'Awesome!', 'james', at_time(9, 30)
+  seamail.reset_read 'kvort'
 end
