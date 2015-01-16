07a38a258219e1aabb55e80b1292a9dd30fb97ec
James Stapleton <github@tasermonkeys.com>
2015-01-15 21:46:27 -0500

    https://github.com/walkeriniraq/twitarr/issues/203 - this passes in the last check time (not including the current time) back to the client.

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -7,6 +7,7 @@ class AlertsController < ApplicationController
   end
 
   def index
+    last_checked_time = current_user[:last_viewed_alerts]
     tweet_mentions = StreamPost.view_mentions(query: current_username,
                                               mentions_only: true).map {|p| p.decorate.to_hash }
     forum_mentions = Forum.view_mentions(query: current_username,
@@ -17,7 +18,8 @@ class AlertsController < ApplicationController
     current_user.reset_last_viewed_alerts
     current_user.save!
     render_json tweet_mentions: tweet_mentions, forum_mentions: forum_mentions,
-                announcements: announcements, unread_seamail: unread_seamail
+                announcements: announcements, unread_seamail: unread_seamail,
+                last_checked_time: (last_checked_time.to_f * 1000).to_i
   end
 
   def check
