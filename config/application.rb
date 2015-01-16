4d3c2bfef21137c528e9ed07ad967ea3823c0d29
Benjamin Reed <ranger@opennms.org>
2015-01-16 12:17:15 -0500

    fix(cors): Fix 404 errors on CORS OPTIONS call for DELETE methods

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Twitarr
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
         origins '*'
-        resource '/api/*', :headers => :any, :methods => [:get, :post, :options]
+        resource '/api/*', :headers => :any, :methods => [:get, :post, :delete, :options]
       end
     end
 
