52b50f4a2e4160daeda378ef511c0b1c1cb6c1b9
James Stapleton <github@tasermonkeys.com>
2015-01-25 18:31:40 -0500

    https://github.com/walkeriniraq/twitarr/issues/224 - Allow the application to hit the alerts endpoint without logging in.  Will keep a session cookie for the non-logged in user so that it can mention the last time that user viewed the alerts

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,22 +1,31 @@
 class AlertsController < ApplicationController
 
-  before_filter :login_required
+  before_filter :login_required, only:[:check]
 
   def login_required
     head :unauthorized unless logged_in?
   end
 
   def index
-    last_checked_time = current_user[:last_viewed_alerts]
-    tweet_mentions = StreamPost.view_mentions(query: current_username,
-                                              mentions_only: true).map {|p| p.decorate.to_hash }
-    forum_mentions = Forum.view_mentions(query: current_username,
-                                              mentions_only: true).map {|p| p.decorate.to_meta_hash }
     announcements = Announcement.valid_announcements.map { |x| x.decorate.to_hash }
-    unread_seamail = current_user.seamails(unread: true).map{|m| m.decorate.to_meta_hash }
+    if logged_in?
+      last_checked_time = current_user[:last_viewed_alerts]
+      tweet_mentions = StreamPost.view_mentions(query: current_username,
+                                                mentions_only: true).map {|p| p.decorate.to_hash(current_username, params) }
+      forum_mentions = Forum.view_mentions(query: current_username,
+                                                mentions_only: true).map {|p| p.decorate.to_meta_hash }
+      unread_seamail = current_user.seamails(unread: true).map{|m| m.decorate.to_meta_hash }
+      current_user.reset_last_viewed_alerts
+      current_user.save!
+    else
+      last_checked_time = session[:last_viewed_alerts] || Time.at(0).to_datetime
+      tweet_mentions = []
+      forum_mentions = []
+      unread_seamail = []
+      session[:last_viewed_alerts] = DateTime.now
+    end
 
-    current_user.reset_last_viewed_alerts
-    current_user.save!
+
     render_json tweet_mentions: tweet_mentions, forum_mentions: forum_mentions,
                 announcements: announcements, unread_seamail: unread_seamail,
                 last_checked_time: (last_checked_time.to_f * 1000).to_i
--- app/controllers/api/v2/alerts_controller.rb
+++ app/controllers/api/v2/alerts_controller.rb
@@ -1,6 +1,6 @@
 class API::V2::AlertsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_filter :login_required
+  before_filter :login_required, only:[:check]
 
 
   def login_required
@@ -8,19 +8,28 @@ class API::V2::AlertsController < ApplicationController
   end
 
   def index
-    tweet_mentions = StreamPost.view_mentions(query: current_username,
-                                              mentions_only: true).map {|p| p.decorate.to_hash }
-    forum_mentions = Forum.view_mentions(query: current_username,
-                                              mentions_only: true).map {|p| p.decorate.to_meta_hash }
     announcements = Announcement.valid_announcements.map { |x| x.decorate.to_hash }
-    unread_seamail = current_user.seamails(unread: true).map{|m| m.decorate.to_meta_hash }
+    if logged_in? || valid_key?(params[:key])
+      last_checked_time = current_user[:last_viewed_alerts]
+      tweet_mentions = StreamPost.view_mentions(query: current_username,
+                                                mentions_only: true).map {|p| p.decorate.to_hash(current_username, params) }
+      forum_mentions = Forum.view_mentions(query: current_username,
+                                                mentions_only: true).map {|p| p.decorate.to_meta_hash }
+      unread_seamail = current_user.seamails(unread: true).map{|m| m.decorate.to_meta_hash }
 
-    unless params[:no_reset]
-      current_user.reset_last_viewed_alerts
-      current_user.save!
+      unless params[:no_reset]
+        current_user.reset_last_viewed_alerts
+        current_user.save!
+      end
+    else
+      last_checked_time = session[:last_viewed_alerts] || Time.at(0).to_datetime
+      tweet_mentions = []
+      forum_mentions = []
+      unread_seamail = []
+      session[:last_viewed_alerts] = DateTime.now unless params[:no_reset]
     end
     render_json tweet_mentions: tweet_mentions, forum_mentions: forum_mentions,
-                announcements: announcements, unread_seamail: unread_seamail
+                announcements: announcements, unread_seamail: unread_seamail, last_checked_time: (last_checked_time.to_f * 1000).to_i
   end
 
   def check
