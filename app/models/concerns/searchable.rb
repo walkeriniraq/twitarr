e52ce7b6016c71a54b517e281672666400e04488
walkeriniraq <nathan@rylath.net>
2015-01-27 23:13:58 -0600

    Commenting out puts

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -13,7 +13,7 @@ module Searchable
     def limit_criteria(criteria, params)
       limit = (params[:limit] || DEFAULT_SEARCH_LIMIT).to_i
       start = (params[:page] || 0).to_i * limit
-      puts "Start = #{start}"
+      # puts "Start = #{start}"
       criteria = criteria.limit(limit)
       criteria = criteria.skip(start) if start > 0
       criteria
