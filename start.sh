3111c46781c3138b9621c7d2f00d2b7fefb31e4b
Jett Jones <jett@jettjones.net>
2015-12-19 15:24:13 -0800

    remove debug code in start.sh

--- start.sh
+++ start.sh
@@ -3,14 +3,6 @@
 set -e
 set -x
 
-# get the lay of the land
-pwd
-ls -al
-env
-
-which rake
-which rails
-
 # docker passes in the linked mongo container by environment variables.
 # plug it into mongoid.yml here
 sed -e "s/127.0.0.1:27017/${MONGO_PORT#tcp://}/" config/mongoid-example.yml > config/mongoid.yml
