a6858cdbcb165c67d10ccfe8dabededf52d378cf
Nathan Walker <nathan@rylath.net>
2016-12-30 14:02:25 -0600

    Made secret tokens work the way they're supposed to in Rails 4

--- config/initializers/secret_token.rb
+++
@@ -1,3 +0,0 @@
-# Put the code below into "secret_token.rb" and replace "some secret token" with the output from "rake secret"
-
-Twitarr::Application.config.secret_key_base = APP_CONFIG["SECRET_TOKEN"]
